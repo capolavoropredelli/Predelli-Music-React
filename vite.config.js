714e8cdb27a5229f5c692e31b3ecaef908dd7863
capolavoro.predelli <capolavoro.predelli@gmail.com>
2026-03-07 10:48:46 +0100

    deply fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'generate-cname',
+      closeBundle() {
+        writeFileSync('dist/CNAME', 'music.predelli.site')
+      }
+    }
+  ],
 })
